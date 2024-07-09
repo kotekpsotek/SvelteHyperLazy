12d2464657b14caed41108bdde9159ecf1d3f152
kotekpsotek <siematumichael@protonmail.com>
2024-07-09 15:49:27 +0200

    Read direcotry functionality modification - Return array with paths to svelte file

--- src/lib/plugin.ts
+++ src/lib/plugin.ts
@@ -1,40 +1,38 @@
 import { type Plugin, normalizePath } from "vite";
-import { readdirSync, lstat } from "fs";
-import { join } from "path";
+import { readdirSync, lstatSync } from "fs";
+import { join, extname } from "path";
+
+type SvelteFilesList = string[]
+
+// TODO: Check syntax by svelte syntax parser as function
 
 /**
  * Checks all units under path under condition if is:
- * * **file**
+ * * **.svelte file**
  * * **directory**
  * 
- * when has encountered **.svelte** file make on it analisis checking svelte syntax whether has **HyperLazy component** and if it is correctly reffered to Svelte file
+ * Returns each encountered Svelte file as array with path to it
  * @param pathToCheck - Location which you'd like to check
  */
-/** TODO: return array with path to all svelte files */
-function checkDir(pathToCheck: string) {
+function checkDir(pathToCheck: string): SvelteFilesList {
     const srcDir = readdirSync(pathToCheck);
+    let filesList: SvelteFilesList = []
 
     for (const subunit of srcDir) {
         // Check unit is file or directory
         const currentPath = join(pathToCheck, subunit);
-        // TODO: Toward collection files and return them outside this function gonna be synchronous one
-        lstat(
-            currentPath, 
-            (err, stats) => {
-                if (err) {
-                    console.error(err)
-                    return;
-                }
-                
-                if (stats.isFile()) {
-                    // TODO: Check file is .svelte one
-                    // TODO: Check syntax by svelte syntax parser
-                } else if (stats.isDirectory()) {
-                    checkDir(currentPath);
-                }
+        const stat = lstatSync(currentPath);
+        
+        if (stat.isFile()) {
+            if (extname(currentPath) == ".svelte") {
+                filesList = [...filesList, currentPath]
             }
-        )
+        } else if (stat.isDirectory()) {
+            filesList = [...filesList, ...checkDir(currentPath)];
+        }
     }
+
+    return filesList;
 }
 
 export default function svelteHyperLazyPlugin(): Plugin<any> {
@@ -48,8 +46,8 @@ export default function svelteHyperLazyPlugin(): Plugin<any> {
             // * Make check only within src directory and all subdirs within
             // * Minimal dependency approach
             const srcDir = join(dir, "src");
-
-            checkDir(srcDir);
+            const svelteFiles = checkDir(srcDir);
+            console.log(svelteFiles)
 
             
             console.log("Build finished!")
